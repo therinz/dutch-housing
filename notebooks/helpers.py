ab47d20a88655c2aca8584e5f1599944e692c20f
Rinze Douma <rjcdouma@gmail.com>
2020-09-30 12:50:18 +0200

    a few more columns

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -17,14 +17,17 @@ def convert_elapsed_time(x):
     return int(days)
 
 
-def extract_num(x, mode):
+def extract_num(col, mode):
     """Return column with integer of number in column."""
 
     pat = {"price": r"€ (\S+)",
            "meter": r"(\d+)(?: m)",
-           "year": r"(\d+)"}
+           "year": r"(\d+)",
+           "rooms": r"(\d+)(?: kamer)",
+           "bedrooms": r"(\d+)(?: slaapkamer)",
+           "floors": r"(\d+)(?: woonla)"}
 
-    return pd.to_numeric(x.astype(str)
+    return pd.to_numeric(col.astype(str)
                          .str.extract(pat[mode], expand=False)
                          .str.replace(".", "")
                          .str.replace(",", ".")
@@ -41,7 +44,7 @@ def build_era(x):
     return int((int(start) + int(end)) / 2)
 
 
-def add_prefix(s, prefix):
+def pref(s, prefix):
     """Add a prefix to distinguish dummies"""
     return f"{prefix}_{s}"
 
@@ -69,14 +72,12 @@ def listing_type(df):
                 if t and t == t}
 
     # Concatenate with original DF
-    pd.concat([df, pd.DataFrame(columns=[add_prefix(t, "pt")
+    pd.concat([df, pd.DataFrame(columns=[pref(t, "pt")
                                          for t in all_tags])], axis=1)
 
     # Evaluate whether tag applicable
     for tag in all_tags:
-        df[add_prefix(tag, "pt")] = np.where(df["property_type"]
-                                             .str.contains(tag, case=False),
-                                             1, 0)
+        df[pref(tag, "pt")] = create_dummy(df["property_type"], tag)
 
     # Cleanup column names
     df.columns = df.columns.str.replace(" ", "_")
@@ -104,16 +105,17 @@ def listing_type(df):
                'tussenwoning': ['geschakelde_woning']}
 
     for key, elem in combine.items():
-        key = add_prefix(key, "pt")
-        elem = [add_prefix(e, "pt") for e in elem]
-        df[key] = np.where(df[[key] + elem].apply(any, axis=1), 1, 0)
+        key = pref(key, "pt")
+        elem = [pref(e, "pt") for e in elem]
+        df[key] = np.where(df[[key] + elem].apply(any, axis=1)
+                           , 1, 0)
 
     # Drop the columns we have combined into others
-    drop = [add_prefix(col, "pt")
+    drop = [pref(col, "pt")
             for lst in combine.values()
             for col in lst]
-    useless = [add_prefix("service_flat", "pt"),
-               add_prefix("bedrijfs-_of_dienstwoning", "pt")]
+    useless = [pref("service_flat", "pt"),
+               pref("bedrijfs-_of_dienstwoning", "pt")]
     df.drop(columns=drop+useless, inplace=True)
 
 
@@ -126,21 +128,17 @@ def roof_description(col):
     roof_type = ["bitumin", "pannen", "kunststof", "leisteen",
                  "metaal", "asbest", "riet"]
 
-    # Create dataframe we will return later
-    dummies = pd.DataFrame()
+    # Create dataframe which we will return later
+    dummies = pd.DataFrame(index=col.index)
 
-    # Create dummies
+    # Create dummy column for each value
     for pat in roof_form:
-        dummies[add_prefix(pat, "rf")] = np.where(col.str.contains(pat,
-                                                                   case=False,
-                                                                   na=False),
-                                                  1, 0)
+        dummies[pref(pat, "rf")] = create_dummy(col, pat)
     for pat in roof_type:
-        dummies[add_prefix(pat, "rt")] = np.where(col.str.contains(pat,
-                                                                   case=False,
-                                                                   na=False),
-                                                  1, 0)
+        dummies[pref(pat, "rt")] = create_dummy(col, pat)
 
     return dummies
 
-test = 3
+
+def create_dummy(col, pattern):
+    return np.where(col.str.contains(pattern, case=False, na=False), 1, 0)
